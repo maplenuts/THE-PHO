a18192021f72c6c37f593017925e0c48848a5f03
maplenuts <hellodwya@gmail.com>
2025-03-10 21:24:02 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,11 +10,6 @@ $(document).ready(function () {
         AOS.init();
     });
 
-    document.querySelectorAll("video").forEach(video => {
-    video.setAttribute("playsinline", "");
-});
-
-
     // Header Scroll Event
     $(function () {
         var prevScrollTop = 0;
